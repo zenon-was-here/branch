a210120733dc8454c364d3082d243de6deb4bc17
zenon-was-here <ze6295@zebra.com>
2024-09-24 15:07:50 -0500

    initial commit

---
+++ src/test/java/org/example/branch/BranchApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.branch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BranchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
