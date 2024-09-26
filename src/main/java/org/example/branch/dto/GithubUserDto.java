04143edca9bb800bdbd72891aaa8de84c9a4acc6
zenon-was-here <ze6295@zebra.com>
2024-09-26 14:54:01 -0500

    clean up

--- src/main/java/org/example/branch/dto/GithubUserDto.java
+++ src/main/java/org/example/branch/dto/GithubUserDto.java
@@ -1,6 +1,5 @@
 package org.example.branch.dto;
 
-import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
